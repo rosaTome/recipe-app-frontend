ac8cffadfe35fb7f7c93f8a458ac222658ad6973
rosaTome <tomerosalinda@gmail.com>
2024-07-23 19:40:01 +0100

    components to fetch data created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
 
 function App() {
+  const [mainRecipes, setMainRecipes] = useState([]);
+  const [dessertRecipes, setDessertRecipes] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch('/api/main-recipes')
+      .then(response => response.json())
+      .then(data => setMainRecipes(data));
+    fetch('/api/dessert-recipes')
+      .then(response => response.json())
+      .then(data => setDessertRecipes(data));
+  }, []);
+
+  const filteredMainRecipes = mainRecipes.filter(recipe => 
+    recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const filteredDessertRecipes = dessertRecipes.filter(recipe => 
+    recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Recipe book</h1>
+      <input 
+      type="text"
+      placeholder="Search Recipes" 
+      value={searchTerm}
+      onChange={ e => setSearchTerm(e.target.value)}
+      />
+      <h2>Main Recipes</h2>
+      <ul>
+        {filteredMainRecipes.map(recipe => (
+          <li key={recipe.id}>{recipe.name}</li>
+        ))}
+      </ul>
+      <h2>Dessert Recipes</h2>
+      <ul>
+        {filteredDessertRecipes.map(recipe => (
+          <li key={recipe.id}>{recipe.name}</li>
+        ))}
+      </ul>
     </div>
-  );
-}
-
-export default App;
+  )
+} 
